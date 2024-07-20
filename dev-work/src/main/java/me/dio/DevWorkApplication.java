8f9f708a0b79a1e0888641f736693bc6ccf7308d
rafaelarreis <rafaelareis7@hotmail.com>
2024-07-20 11:20:34 +0100

    Commit inicial

---
+++ dev-work/src/main/java/me/dio/DevWorkApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWorkApplication.class, args);
+	}
+
+}
